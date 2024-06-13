49a5e98d6ea45365a712a81fe05317cf7d3fb0f8
JonaDielmann <jona.dielmann@deutschebahn.com>
2024-06-13 19:17:26 +0200

    Lösung für Aufgabe_1 hinzugefügt

--- Aufgabe_1/src/FizzBuzz.java
+++ Aufgabe_1/src/FizzBuzz.java
@@ -1,6 +1,22 @@
 public class FizzBuzz {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
     static String of(int input) {
-        //Ab hier kannst du die Funktion anpassen ;)
-        return null;
+        String result = "";
+        if(isFactorOf(input, 3)){
+            result += FIZZ;
+        }
+
+        if(isFactorOf(input, 5)){
+            result += BUZZ;
+        }
+
+        return result.isEmpty() ? String.valueOf(input) : result;
     }
+
+    private static boolean isFactorOf(int number, int factor) {
+        return number % factor == 0;
+    }
+
 }
--- Aufgabe_1/src/FizzBuzzTest.java
+++ Aufgabe_1/src/FizzBuzzTest.java
@@ -9,4 +9,48 @@ public class FizzBuzzTest {
         assertEquals("1", FizzBuzz.of(1));
     }
 
+    @Test
+    public void whenIs2_thenReturn2() {
+        assertEquals("2", FizzBuzz.of(2));
+    }
+
+    @Test
+    public void whenIs3_thenReturnFizz() {
+        assertEquals("Fizz", FizzBuzz.of(3));
+    }
+
+    @Test
+    public void whenIs4_thenReturn4() {
+        assertEquals("4", FizzBuzz.of(4));
+    }
+
+    @Test
+    public void whenIs5_thenReturnBuzz() {
+        assertEquals("Buzz", FizzBuzz.of(5));
+    }
+
+    @Test
+    public void whenIs6_thenReturnFizz() {
+        assertEquals("Fizz", FizzBuzz.of(6));
+    }
+
+    @Test
+    public void whenIs10_thenReturnBuzz() {
+        assertEquals("Buzz", FizzBuzz.of(10));
+    }
+
+    @Test
+    public void whenIs15_thenReturnFizzBuzz() {
+        assertEquals("FizzBuzz", FizzBuzz.of(15));
+    }
+
+    @Test
+    public void whenIs99_thenReturnFizz() {
+        assertEquals("Fizz", FizzBuzz.of(99));
+    }
+
+    @Test
+    public void whenIs100_thenReturnFizz() {
+        assertEquals("Buzz", FizzBuzz.of(100));
+    }
 }
