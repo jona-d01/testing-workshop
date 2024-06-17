16efe582507be2284ebd9493f4cffb6d2f5ba9b7
JonaDielmann <jona.dielmann@deutschebahn.com>
2024-06-17 20:09:15 +0200

    Lösung für Aufgabe_2 hinzugefügt

--- Aufgabe_2/tests/example.spec.js
+++ Aufgabe_2/tests/example.spec.js
@@ -10,19 +10,23 @@ test('page should have title', async ({ page }) => {
 test('page should have english title when english is the selected language', async ({ page }) => {
   await page.goto('https://www.th-koeln.de/');
 
-  /* TODO: Teste, ob der Titel der Seite sich zu 'Home – TH Köln' ändert, nachdem man die Sprache auf der Seite auf 'English' umstellt */
+  await page.locator('.langswitch').getByText('English').click()
+
+  await expect(page).toHaveTitle('Home - TH Köln');
 });
 
 test('page should have image with title', async ({ page }) => {
   await page.goto('https://www.th-koeln.de/');
 
-  /* TODO: Teste, ob das Bild mit dem Titel 'Farbenfroh beleuchtetes Ingenieurwissenschaftliches Zentrum am Campus Deutz (Bild: Thilo Schmülgen/TH Köln)' auf der Startseite sichtbar ist*/
+  const image = await page.getByTitle('Farbenfroh beleuchtetes Ingenieurwissenschaftliches Zentrum am Campus Deutz (Bild: Thilo Schmülgen/TH Köln)');
+
+  await expect(image).toBeVisible();
 });
 
 test('page should have link with color', async ({ page }) => {
   await page.goto('https://www.th-koeln.de/');
 
-  /* TODO: Teste, ob der Navigationslink unter dem Selektor '.menu_container' -> '.active' die Farbe rgb(198, 12, 15) besitzt */
+  await expect(page.locator('.menu_container').locator('.active')).toHaveCSS('color','rgb(198, 12, 15)');
 });
 
 
@@ -31,11 +35,19 @@ test('page should have link with color', async ({ page }) => {
 test('page should take screenshot of image', async ({ page }) => {
   await page.goto('https://www.th-koeln.de/');
 
-  /* TODO: Mache einen Screenshot von dem vorher gesuchten Bild */
+  const image = await page.getByTitle('Farbenfroh beleuchtetes Ingenieurwissenschaftliches Zentrum am Campus Deutz (Bild: Thilo Schmülgen/TH Köln)');
+
+  await image.screenshot({ path: 'screenshot.png' });
+
+  await expect(image).toBeVisible();
 });
 
 test('page should show mail', async ({ page }) => {
   await page.goto('https://www.th-koeln.de/');
 
-  /* TODO: Teste, ob bei einer Suche nach dem Wort 'Alexander' die Mailadresse 'alexander.dobrynin@th-koeln.de' zu finden ist */
+  await page.locator('#searchInput').fill('Alexander');
+
+  await page.keyboard.down('Enter');
+
+  await expect(page.locator('.result')).toContainText('alexander.dobrynin@th-koeln.de');
 });
